d7d45dcb2ce3b3917ccba8ca020986ea4bed4b71
Basti Buck <mail@bastibuck.de>
2022-01-29 18:54:23 +0100

    update docs text

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -4,6 +4,8 @@ export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
+      <h2>This might become a documentation for twitch-viz. It might.</h2>
+      <p>We all know it never gonna</p>
       <Button />
     </div>
   );
