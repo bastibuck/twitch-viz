4a3a1ad0738723665a59962e9a51d54097e02c96
Basti Buck <mail@bastibuck.de>
2022-01-29 23:16:16 +0100

    keep connection for active clients

--- apps/backend/src/client/client.service.ts
+++ apps/backend/src/client/client.service.ts
@@ -1,22 +1,74 @@
 import { Injectable } from "@nestjs/common";
+import * as tmi from "tmi.js";
 
 import { NewClientInput } from "./client.input";
 import { Client } from "./client.model";
 
+const CHECK_ACTIVE_INTERVAL = 1000 * 60 * 2;
+const INACTIVE_TIME = 1000 * 60 * 5;
+
 @Injectable()
 export class ClientService {
-  private activeClients: { id: string; lastPing: number }[] = [];
+  private activeClients: {
+    id: string;
+    lastPing: number;
+    interval: NodeJS.Timeout;
+  }[] = [];
 
   async isClientActive(clientId: string): Promise<boolean> {
-    return this.activeClients.some((client) => client.id === clientId);
+    const activeClient = this.activeClients.find(
+      (client) => client.id === clientId,
+    );
+
+    if (!activeClient) {
+      return false;
+    }
+
+    this.activeClients = this.activeClients.map((client) => ({
+      ...client,
+      lastPing: new Date().valueOf(),
+    }));
+
+    return true;
   }
 
   async create(data: NewClientInput): Promise<Client> {
+    const TMI = new tmi.Client({
+      channels: [data.channelName],
+    });
+
+    TMI.connect();
+
+    TMI.on("message", (channel, tags, message, self) => {
+      console.log(`${tags["display-name"]}: ${message}`);
+    });
+
     const client = new Client(data);
+
+    const interval = setInterval(() => {
+      const activeClient = this.activeClients.find(
+        (activeClient) => activeClient.id === client.id,
+      );
+
+      if (!activeClient) {
+        TMI.disconnect();
+        clearInterval(interval);
+      }
+
+      if (activeClient.lastPing < new Date().valueOf() - INACTIVE_TIME) {
+        this.activeClients = this.activeClients.filter(
+          (activeClient) => activeClient.id !== client.id,
+        );
+
+        TMI.disconnect();
+        clearInterval(interval);
+      }
+    }, CHECK_ACTIVE_INTERVAL);
 
     this.activeClients.push({
       id: client.id,
       lastPing: client.lastPing.valueOf(),
+      interval,
     });
 
     return client;
